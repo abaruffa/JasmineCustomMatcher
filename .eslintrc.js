0155ab70c0139fd46340ab1e1873828419c1cd1a
Baruffa <amberbaruffa@gmail.com>
2016-04-27 21:22:05 -0400

    Set up linting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "jasmine": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "eqeqeq": [
+            "error",
+            "smart"
+        ],
+        "max-len": [
+            "error",
+            80
+        ]
+    }
+};
